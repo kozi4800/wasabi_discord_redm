a02d0f255d87995d075796ae92ef3465ae0b79b9
Suu <takeshi.soufiane@gmail.com>
2024-10-27 14:35:47 +0100

    Update fxmanifest.lua
    convert to redm

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,11 +3,13 @@
 ---------------------------------------------------------------
 
 fx_version 'cerulean'
-game 'gta5'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+
 lua54 'yes'
 
-description 'Wasabi Extended Discord Wrapper'
-version '1.0.8'
+description 'Wasabi Extended Discord Wrapper for RedM'
+version '1.0.7'
 
 client_scripts {
   'client/*.lua'
